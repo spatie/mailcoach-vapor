8bd850a35c0ac526a2696e5838fc31abdb9c5157
freek <freek@spatie.be>
2021-10-04 11:02:53 +0200

    wip

---
+++ README.md
@@ -0,0 +1,23 @@
+# Welcome to Mailcoach
+
+[Mailcoach](https://mailcoach.app) is a self-hosted email list manager - in a modern jacket.
+
+It features:
+- Subscribers and lists management
+- Subscribe, double opt-in and unsubscribe flows
+- HTML templates with drop-in variables
+- Drafts, previews and test mails
+- Send newsletters to an unlimited amount of subscribers
+- Statistics and tracking of opens and clicks
+
+In order to be able to install all necessary dependencies you need an mailcoach license you can buy at [mailcoach.app](https://mailcoach.app)
+
+Read our documentation on [how to get started](https://mailcoach.app/docs). 
+
+## Support us
+
+[<img src="https://github-ads.s3.eu-central-1.amazonaws.com/mailcoach.jpg?t=1" width="419px" />](https://spatie.be/github-ad-click/Mailcoach)
+
+We invest a lot of resources into creating [best in class open source packages](https://spatie.be/open-source). You can support us by [buying one of our paid products](https://spatie.be/open-source/support-us).
+
+We highly appreciate you sending us a postcard from your hometown, mentioning which of our package(s) you are using. You'll find our address on [our contact page](https://spatie.be/about-us). We publish all received postcards on [our virtual postcard wall](https://spatie.be/open-source/postcards).
---
+++ app/Console/Kernel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->command('mailcoach:calculate-statistics')->everyMinute();
+        $schedule->command('mailcoach:send-scheduled-campaigns')->everyMinute();
+        $schedule->command('mailcoach:send-campaign-summary-mail')->hourly();
+        $schedule->command('mailcoach:send-email-list-summary-mail')->mondays()->at('9:00');
+        $schedule->command('mailcoach:delete-old-unconfirmed-subscribers')->daily();
+        $schedule->command('mailcoach:cleanup-processed-feedback')->hourly();
+        $schedule->command('mailcoach:run-automation-triggers')->everyMinute()->runInBackground();
+        $schedule->command('mailcoach:run-automation-actions')->everyMinute()->runInBackground();
+        $schedule->command('mailcoach:calculate-automation-mail-statistics')->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
